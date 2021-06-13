52485c8443d857139e2be7706293f242011340c4
ShadiestGoat <shady@shadygoat.eu>
2021-06-13 19:30:25 +0100

    testin

--- src/index.ts
+++ src/index.ts
@@ -122,7 +122,7 @@ app.post('/upload', async (req, res) => {
             const info2 = new authorModel(_info2)
             await info2.save()
             usr.submitted.push(id)
-            await userModel.findOneAndReplace({id: usr.id}, usr)
+            await userModel.findOneAndUpdate({id: usr.id}, usr)
             cache[id] = _info2
             acache[usr.id] = usr
 
